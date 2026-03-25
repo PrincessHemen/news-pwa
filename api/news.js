d7e64e0632cae66171c9895d6ba23f8f3f0aed20
Dooshima Hemen <hemendprincess@gmail.com>
2026-03-25 16:01:57 +0100

    Removed vercel.json

--- api/news.js
+++ api/news.js
@@ -5,6 +5,8 @@ export default async function handler(req, res) {
       `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${process.env.NEWS_API_KEY}`
     );
   
+    console.log("API KEY:", process.env.NEWS_API_KEY);
+    
     const data = await response.json();
   
     res.status(200).json(data);
